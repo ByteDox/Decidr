85fb371b5c089229e2edcfcf99ec9fe079e88598
ByteDox <64937191+ByteDox@users.noreply.github.com>
2025-07-13 22:28:48 +1000

    v1.1

--- main.py
+++ main.py
@@ -7,12 +7,12 @@ def pick_number():
         fNum = int(fNum_var.get())
         sNum = int(sNum_var.get())
         if fNum > sNum:
-            result_label.config(text = "First Number must be less than Second Number.")
+            result.config(text = "First Number must be less than Second Number.")
             return
         number = random.randint(fNum, sNum)
-        result_label.config(text = f"Your pick is: {number}")
+        result.config(text = f"Your pick is: {number}")
     except ValueError:
-        result_label.config(text = "Error: Please enter a valid number!")
+        result.config(text = "Error: Please enter a valid number!")
 
 
 root = tk.Tk()
@@ -40,15 +40,15 @@ ttk.Label(content_frame, text = "Pick a Number Between: ").pack(pady = (0, 10))
 entry_frame = ttk.Frame(content_frame)
 entry_frame.pack()
 
-ttk.Label(entry_frame, text="First Number: ").grid(row = 0, column = 0, padx = 5, sticky = "e")
+ttk.Label(entry_frame, text="First Number:").grid(row = 0, column = 0, padx = 5, sticky = "e")
 ttk.Entry(entry_frame, textvariable = fNum_var, width = 10).grid(row = 0, column = 1)
 
-ttk.Label(entry_frame, text="Second Number: ").grid(row = 1, column = 0, padx = 5, sticky = "e")
+ttk.Label(entry_frame, text="Second Number:").grid(row = 1, column = 0, padx = 5, sticky = "e")
 ttk.Entry(entry_frame, textvariable = sNum_var, width = 10).grid(row = 1, column = 1)
 
 ttk.Button(content_frame, text = "Generate", command = pick_number).pack(pady = 15)
 
-result_label = ttk.Label(content_frame, text = "", foreground = "#333", font = ("Segoe UI", 11, "bold"))
-result_label.pack()
+result = ttk.Label(content_frame, text = "", foreground = "#333", font = ("Segoe UI", 11, "bold"))
+result.pack()
 
 root.mainloop()
